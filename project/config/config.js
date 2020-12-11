cc08784eef342e15dfe2ba4ac21fca7201c68240
Max Muller <max.muller@aalto.fi>
2020-12-11 18:39:47 +0200

    New configs

--- project/config/config.js
+++ project/config/config.js
@@ -4,10 +4,10 @@ if (Deno.env.get('TEST_ENVIRONMENT')) {
   config.database = {};
 } else {
   config.database = {
-    hostname: "ec2-54-170-190-29.eu-west-1.compute.amazonaws.com",
-    database: "d3p3po5294tocd",
-    user: "khelrcnrjognzd",
-    password: "7ae89722f11fada07e27ada78efaa508443421a8b76a91e5a19e36a173758400",
+    hostname: "http://kandula.db.elephantsql.com",
+    database: "zczukuha",
+    user: "zczukuha",
+    password: "FK8tGoTvdEy1m5UqyT8VTtWIIY2r3pqi",
     port: 5432
   };
 }
